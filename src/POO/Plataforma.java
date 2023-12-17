79311d696808df4e55111d11202f63d0c9a15712
David <David@DESKTOP-REDOD6I>
2023-12-16 23:31:40 -0600

    Carga Inicial

--- src/POO/Plataforma.java
+++ src/POO/Plataforma.java
@@ -11,26 +11,61 @@ public class Plataforma { // la plataforma es la clase: caracteristicas comunes 
    private int largo;
    private int ancho;
    private int motor;
-   private int peso;
-    
+   private int peso_plataforma;
+   
+   //propiedades que pueden variar
+   
+   String color;
+   int peso_total;
+   boolean asientos_cuero, climatizador;
 
     /*Paso 2)
     especificar los valores de las caracteristicas creando un metodo constructor metodo constructor es el que se encarga de dar un estado inicial
     el metodo constructor tiene el msimo nombre de la clase*/ 
+   
+    /*solo desde esta clase puedo cambiar los valores de la plataforma
+      eso se llama modularización para eso se necesitan los modificadores
+      de acceso "private" "public" "protected"
+    */
     
     public Plataforma(){
         ruedas=4;
         largo = 2000;
         ancho = 300;
         motor = 1600;
-        peso = 500;
+        peso_plataforma = 500;        
+        
     }
 
     /*Paso 3) crear la clase principal donde se usa esta clase*/
     
-    /*solo desde esta clase puedo cambiar los valores de la plataforma
-      eso se llama modularización para eso se necesitan los modificadores
-      de acceso "private" "public" "protected"
+    
+    /*Paso 4)
+    la forma de comunicarse entre clases son metodos
+        
+    Setter = se modifica el valor de una propiedad
+            public void nombre_metodo(){codigo sin return}
+    Getter = obtenemos el valor de la propiedad
+            public tipo_de_dato nombre_metodo(){codigo + return}
     */
+    
+    
+    //METODOS GETTER
+    
+      public String obtener_ruedas(){
+         
+          return "El numero de ruedas del coche son: "+ruedas;
+      };
+    
+    //METODOS SETTER
+      public void Establecer_color(){
+          color="Azul";
+      }
+    
+    //METODO GETTER
+      
+      public String Obtener_color(){
+          return "El color del coche es: "+color;
+      }
 }
 
--- src/POO/Uso_plataforma.java
+++ src/POO/Uso_plataforma.java
@@ -10,17 +10,23 @@ public class Uso_plataforma {
         Clase que se usa . nombre del objeto = new nombre del constructor ();
         */
         Plataforma Renault = new Plataforma();
-    
+        Plataforma Suzuki = new Plataforma();
+        
         /* Paso 4)
             Acceder a las propiedades = nombre del objeto . propiedad
                  System.out.println("Este coche tiene "+ Renault.ruedas + " ruedas");
         
-            Acceder a las propuedades con private = 
-                para acceder se debe acceder por medio de un metodo
+            Acceder a las propuedades con private = hay que llamarlo desde el metodo creado en la otra clase
+                para acceder se debe acceder por medio de un metodo setters y getters
         */
        //  Renault.ruedas=3; <------ esto no puede suceder entonces tiene que estar modularizado
        
-     
+       System.out.println(Renault.obtener_ruedas());
         
+       //modificar el color de un objeto estableciendolo desde el metodo setter
+       Suzuki.Establecer_color();
+       
+       //imprimir el color
+       System.out.println(Suzuki.Obtener_color());
     }
 }
